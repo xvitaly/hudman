a04a936feb386e1405ca60d6cb0716abc476ef24
Vitaly Zaitsev <vitaly@easycoding.org>
2019-09-22 16:30:02 +0200

    Added --save command-line option.

--- hudman/app/run.py
+++ hudman/app/run.py
@@ -27,11 +27,16 @@ def main():
     parser = ArgumentParser()
     parser.add_argument('--huddb', '-d', help='Specify path to HUDs database file.', required=True)
     parser.add_argument('--outdir', '-o', help='Specify path to save downloaded files.', required=True)
+    parser.add_argument('--save', '-s', help='Automatically save changes in HUD database file.', action='store_true', required=False)
     cmdline = parser.parse_args()
 
     # Run mirror script...
     hm = HUDMirror(cmdline.huddb, cmdline.outdir)
     hm.getall()
+
+    # Saving changes if enabled...
+    if cmdline.save:
+        hm.save()
 
 
 if __name__ == '__main__':
