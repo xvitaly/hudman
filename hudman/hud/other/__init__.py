59bcd555a5a1e83340da14cd6b274972bee36262
Vitaly Zaitsev <vitaly@easycoding.org>
2021-04-09 16:43:44 +0200

    Implemented HUDOther class.

---
+++ hudman/hud/other/__init__.py
@@ -0,0 +1,32 @@
+# coding=utf-8
+
+# SPDX-FileCopyrightText: 2016-2021 EasyCoding Team
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+import urllib.request
+
+from ...hudmsg import HUDMessages, HUDSettings
+from ...headertime import HeaderTime
+from ...hud import HUDCommon
+
+
+class HUDOther(HUDCommon):
+    def __checkhead(self) -> int:
+        """
+        Call HTTP HEAD method to retrieve last modification time
+        of specified URL.
+        :param url: URL of remote file.
+        :return: Last modification time.
+        :rtype: int
+        """
+        request = urllib.request.Request(self.upstreamuri, data=None, headers={'User-Agent': HUDSettings.ua_curl}, method='HEAD')
+        response = urllib.request.urlopen(request)
+        if response.status != 200:
+            raise Exception(HUDMessages.oth_errcode.format(response.status))
+        headers = response.info()
+        return HeaderTime.hth2unix(headers['Last-Modified'])
+
+    def check(self):
+        self._updatecheck = self.__checkhead()
+        return self._updatecheck > self.lastupdate
