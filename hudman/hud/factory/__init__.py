219b4d7de8812c4ce20d69090d75ee56c42d3865
Vitaly Zaitsev <vitaly@easycoding.org>
2021-06-12 15:52:03 +0200

    Moved HUDFactory class to a separate package.
    Closes #51.

--- hudman/hud/factory/__init__.py
+++ hudman/hud/factory/__init__.py
@@ -4,8 +4,8 @@
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-from .github import HUDGitHub
-from .other import HUDOther
+from ..github import HUDGitHub
+from ..other import HUDOther
 
 
 class HUDFactory:
