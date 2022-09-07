db911cce360d51f6e5eef270303b7d369df3717e
Vitaly Zaitsev <vitaly@easycoding.org>
2022-09-07 18:36:10 +0200

    Implemented custom exception types.
    Closes #73.

---
+++ hudman/exceptions.py
@@ -0,0 +1,13 @@
+# coding=utf-8
+
+# SPDX-FileCopyrightText: 2016-2022 EasyCoding Team
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+__all__ = ['ArchiveNotValid']
+
+
+class ArchiveNotValid(Exception):
+    """
+    Base class for the archive validation errors.
+    """
